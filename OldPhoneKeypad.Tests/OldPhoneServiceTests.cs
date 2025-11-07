92f11a76a75d7b9de045df5eb6bdef26d940677b
Thu Yein <thuyein1996alpha@gmail.com>
2025-11-07 21:24:33 +0700

    [add] unit test

---
+++ OldPhoneKeypad.Tests/OldPhoneServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using OldPhoneKeypad.Services;
+
+namespace OldPhoneKeypad.Tests;
+
+public class OldPhoneServiceTests
+{
+    [Theory]
+    [InlineData("33#", "E")]
+    [InlineData("227*#", "B")]
+    [InlineData("4433555 555666#", "HELLO")]
+    [InlineData("8 88777444666*664#", "TURING")]
+    public void ConvertInput_ShouldReturnExpectedText(string input, string expected)
+    {
+        var result = OldPhoneService.ConvertInput(input);
+        Assert.Equal(expected, result);
+    }
+}
